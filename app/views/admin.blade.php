46de78a45649f97dafb4b064e3366978caf323ed
Stefan Ledin <stefan.ledin@gmail.com>
2013-04-20 15:54:25 +0200

    Inloggningsformulär visas om man inte är inloggad. Länk för att logga ut visas om man är inloggad.

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -1,13 +1,25 @@
 @extends('master')
 
+
+@if (! Auth::check() )
+
+<h2>Logga in</h2>
+
+	{{ Form::open(array('url' => 'admin/login')) }}
+		<?php echo Form::token(); ?>
+		<?php echo Form::label('username', 'Användarnamn'); ?>
+		<?php echo Form::text('username'); ?>
+		<?php echo Form::label('password', 'Lösenord'); ?>
+		<?php echo Form::password('password'); ?>
+	<?php echo Form::submit('Logga in'); ?>
+	{{ Form::close() }}
+
+@else
+
 <h2>Lägg till tips</h2>
-{{ Form::open(array('url' => 'admin/login')) }}
-   <?php echo Form::label('name', 'Namn'); ?>
-   <?php echo Form::text('name'); ?>
-   <?php echo Form::label('phone', 'Telefonnummer'); ?>
-   <?php echo Form::text('phone'); ?>
-   <?php echo Form::submit('Logga in'); ?>
-{{ Form::close() }}
+<a href="admin/logout">Logga ut</a>
+
+@endif
 
 <h2>Facit</h2>
 /*
