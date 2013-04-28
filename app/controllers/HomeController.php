91e944abbfaeec9b559ab658081eb67525865710
Stefan Ledin <stefan.ledin@gmail.com>
2013-04-28 12:17:02 +0200

    Arrayen med matcher är extraherad till en egen helper-class

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -20,26 +20,7 @@ class HomeController extends BaseController {
 		$row = DB::table('result')->first();
 		$row = unserialize($row->row);
 
-		$games = [
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'IF Örnen'],
-			['Hemmalag' => 'Jössefors IK', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Bäckalunds IF', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Lysviks IF'],
-			['Hemmalag' => 'Gettjärns IF', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Gunnarskog/Bortan IK'],
-			['Hemmalag' => 'Torsby IF U', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Rännbergs IK'],
-			['Hemmalag' => 'Åmotfors IF/IFK Ås', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'IF Örnen', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Jössefors IK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Bäckalunds IF'],
-			['Hemmalag' => 'Lysviks IF', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Gettjärns IF'],
-			['Hemmalag' => 'Gunnarskog/Bortan IK', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Torsby IF U'],
-			['Hemmalag' => 'Rännbergs IK', 'Bortalag' => 'Älgå SK'],
-			['Hemmalag' => 'Älgå SK', 'Bortalag' => 'Åmotfors IF/IFK Ås']
-		];
+		$games = Games::getGames();
 		return View::make('admin')
 			->with('games', $games)
 			->with('row', $row);
